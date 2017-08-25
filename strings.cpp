5066ce583fd82e76708525c28bd4397f2d43b5c8
Huang Gaofeng <csuhgf@hotmail.com>
2017-08-25 16:01:10 +0800

    has no change

--- strings.cpp
+++ strings.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
 
 //const int BUFSIZ = 100;
@@ -31,6 +30,7 @@ int main(int argc,char *argv[])
 	int i;
 	FILE *fin;
 //	setprogname("strings_hgf");
+	printf ("the number of arg is: %d \n",argc);	/* for a verify */
 	if (argc == 1)
 		printf("usage: strings filename");
 	else {
